268ec2a1eb5f15bd2ed8d54e2b1a9f40818bdc74
NetGeoOWild <goshan397@gmail.com>
2026-03-20 19:43:56 +0300

    add example task in app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
 import { computed } from 'vue'
 import { darkTheme, NConfigProvider, NGlobalStyle } from 'naive-ui'
 import { useTheme } from '@/composable/useTheme'
+import { useTaskStore } from './stores/taskStore'
 import { lightThemeOverrides, darkThemeOverrides } from './configs/appConfig'
 import AppMain from './layout/AppMain.vue'
 
 const { isDark } = useTheme()
+
 const currentOverrides = computed(() => {
   return isDark.value ? darkThemeOverrides : lightThemeOverrides
 })
+
+// Для примера что-бы список туду не был пустым
+const { addTask } = useTaskStore()
+addTask("Покормить кота")
+addTask("Посмотреть мультфильм Шрек")
+addTask("Сходить в спортивный зал")
 </script>
 
 <template>
--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -12,31 +12,7 @@ export const useTaskStore = defineStore(
   'task',
   () => {
     // sate
-    const tasks = ref<Tasks[]>([
-      {
-        // для живого примера
-        id: crypto.randomUUID(),
-        text: 'Покормить кота',
-        completed: false,
-        createdAt: new Date().toString(),
-      },
-
-      {
-        // для живого примера
-        id: crypto.randomUUID(),
-        text: 'Посмотреть мультфильм - Шрек',
-        completed: false,
-        createdAt: new Date().toString(),
-      },
-
-      {
-        // для живого примера
-        id: crypto.randomUUID(),
-        text: 'Сходить в спортинвый зал',
-        completed: false,
-        createdAt: new Date().toString(),
-      },
-    ])
+    const tasks = ref<Tasks[]>([])
     const currentFilter = ref<'all' | 'completed' | 'not-completed'>('all')
 
     const filteredTasks = computed(() => {
